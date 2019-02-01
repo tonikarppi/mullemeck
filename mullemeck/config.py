f896c0db96846c1bc3fd640e11d3d6c932ce484d
Alexander Westberg-Bladh <alexander.bladh@gmail.com>
2019-02-01 16:54:11 +0100

    Add function for Reading config

--- mullemeck/config.py
+++ mullemeck/config.py
@@ -1,22 +1,58 @@
-
-CONFIG_FILE = "mulle.cfg"
+import configparser
+from pathlib import Path
+CONFIG_FILE_PATH = "mulle.cfg"
 
 DB_READ_USER = ""
 DB_READ_USER_PASSWORD = ""
 DB_WRITE_USER = ""
-DB_WRITE_USER_PASSWIRD = ""
+DB_WRITE_USER_PASSWORD = ""
 
 CLONE_DIRECTORY = "/tmp/mulle/"
 
+
 def read():
-    pass
+    """
+        Reads a config from `CONFIG_FILE_PATH`
+        If it does not exist a default config is written
+    """
+    global DB_READ_USER
+    global DB_READ_USER_PASSWORD
+    global DB_WRITE_USER
+    global DB_WRITE_USER_PASSWORD
+    global CLONE_DIRECTORY
+
+    config = configparser.ConfigParser()
+    config_file = Path(CONFIG_FILE_PATH)
+    if not config_file.is_file():
+        print("No config Found in {}. Writing defaults"
+              .format(CONFIG_FILE_PATH))
+        config["DEFAULT"] = {
+            "DBReadUser": "",
+            "DBReadUserPassword": "",
+            "DBWriteUser": "",
+            "DBWriteUserPassword": "",
+            "CloneDirectory": CLONE_DIRECTORY
+        }
+        with open(CONFIG_FILE_PATH, 'w') as configfile:
+            config.write(configfile)
+    else:
+        config.read(CONFIG_FILE_PATH)
+        DB_READ_USER = config["DEFAULT"]["DBReadUser"]
+        DB_READ_USER_PASSWORD = config["DEFAULT"]["DBReadUserPassword"]
+        DB_WRITE_USER = config["DEFAULT"]["DBWriteUser"]
+        DB_WRITE_USER_PASSWORD = config["DEFAULT"]["DBWriteUserPassword"]
+        CLONE_DIRECTORY = config["DEFAULT"]["CloneDirectory"]
+
+    return validate()
+
 
 def validate():
-    pass
+    return True
+
 
 def get_db_user():
     pass
 
+
 def get_db_write_user():
     pass
-
---
+++ tests/test_config.py
@@ -0,0 +1,53 @@
+import configparser
+from mullemeck import config
+from pathlib import Path
+import subprocess
+
+"""
+    Verifies that config reads correctly,
+    And that clone directory is created
+"""
+
+tmp_file = ".tmp_config"
+original_file = ""
+
+
+def setup_module(module):
+    "Changes config file for testing"
+    global original_file
+    original_file = config.CONFIG_FILE_PATH
+    config.CONFIG_FILE_PATH = tmp_file
+
+
+def teardown_module(module):
+    "reverts to old test file"
+    subprocess.run(["rm", config.CONFIG_FILE_PATH])
+    config.CONFIG_FILE_PATH = original_file
+
+
+def test_create_config():
+    config.read()
+    assert Path(config.CONFIG_FILE_PATH).is_file()
+
+
+def test_read_config():
+    writtenConfig = configparser.ConfigParser()
+    data = {
+        "DBReadUser": "abc",
+        "DBReadUserPassword": "123",
+        "DBWriteUser": "cde",
+        "DBWriteUserPassword": "123",
+        "CloneDirectory": "/tmp/test"
+    }
+    writtenConfig["DEFAULT"] = data
+
+    with open(tmp_file, 'w') as configfile:
+        writtenConfig.write(configfile)
+
+    config.CONFIG_FILE_PATH = tmp_file
+    config.read()
+    assert config.DB_READ_USER == data["DBReadUser"]
+    assert config.DB_READ_USER_PASSWORD == data["DBReadUserPassword"]
+    assert config.DB_WRITE_USER == data["DBWriteUser"]
+    assert config.DB_WRITE_USER_PASSWORD == data["DBWriteUserPassword"]
+    assert config.CLONE_DIRECTORY == data["CloneDirectory"]
