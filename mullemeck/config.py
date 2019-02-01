840ebe07eba37559d1f4b7eedf7332be42149608
Alexander Westberg-Bladh <alexander.bladh@gmail.com>
2019-02-01 15:49:00 +0100

    Add config variables and skeleton funcitons

---
+++ mullemeck/config.py
@@ -0,0 +1,22 @@
+
+CONFIG_FILE = "mulle.cfg"
+
+DB_READ_USER = ""
+DB_READ_USER_PASSWORD = ""
+DB_WRITE_USER = ""
+DB_WRITE_USER_PASSWIRD = ""
+
+CLONE_DIRECTORY = "/tmp/mulle/"
+
+def read():
+    pass
+
+def validate():
+    pass
+
+def get_db_user():
+    pass
+
+def get_db_write_user():
+    pass
+
