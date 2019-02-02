b6c92463cd7f68dcc8725cef090950717951e56d
Toni Karppi <karppitoni@gmail.com>
2019-02-02 10:21:41 +0100

    Implement hello world flask server

---
+++ mullemeck/backend/__main__.py
@@ -0,0 +1,11 @@
+from mullemeck.backend import app
+
+
+def main(args):
+    debug = 'debug' in args
+    app.run(debug=debug)
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
