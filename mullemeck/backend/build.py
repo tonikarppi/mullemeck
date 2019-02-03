b435c630e84eeca450637e10320bdfcc9bc7cbf4
SandstormVR <alexis.hubert@student.ecp.fr>
2019-02-03 21:58:52 +0000

    Fixed issue with pre-commit.log folder location.

--- mullemeck/backend/build.py
+++ mullemeck/backend/build.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 
 def build_static_checks(project):
@@ -8,21 +9,20 @@ def build_static_checks(project):
     It assumes that the project will be cloned in projects/ and that the file
     is run from root level of the mullemeck project.
     """
+    path_to_mullemeck = os.getcwd()
     # Change directory to the project
     command1 = 'cd ./projects/' + project
     # runs pre-commit on all the files with the local configuration
     command2 = 'pre-commit run -a -c ./.pre-commit-config.yaml'
     # Saves the results of the combine two previous commands in pre-commit.log
-    subprocess.call('touch pre-commit.log', shell=True)
-    subprocess.call(command1 + '&&' + command2 +
-                    '>../../pre-commit.log', shell=True)
+    subprocess.call(command1 + '&&' + command2 + '>' +
+                    path_to_mullemeck + '/pre-commit.log', shell=True)
     # Reads the log file
     with open('pre-commit.log') as file:
         lines = file.readlines()
 
     # Creates a single string variable for all the logs
     logs = ' '.join(lines)
-
     # If a test is not succesfuly run, 'failde' appears in the last line of the
     # logs.
     status = True
