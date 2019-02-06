c7dc1f95dfb17e58b9b25dfe20b7f771a28f4fac
Toni Karppi <karppitoni@gmail.com>
2019-02-06 10:31:10 +0100

    Add add_samples option to main

--- mullemeck/__init__.py
+++ mullemeck/__init__.py
@@ -1,14 +1,17 @@
 from mullemeck.routes import app
 from .db import create_tables
 from .settings import validate_environment_variables
+from .utils import add_samples_to_db
 
 
 def main(*args):
     validate_environment_variables()
     assert len(args) == 1, 'Expected 1 argument <setup|develop|production>'
     arg = args[0]
-    if arg == 'setup':
+    if arg == 'create_tables':
         create_tables()
+    if arg == 'add_samples':
+        add_samples_to_db()
     elif arg == 'develop':
         app.run(debug=True)
     elif arg == 'production':
