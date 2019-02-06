c0010696690441c9ae5c895d5ab5bbbd2ee96b1d
Toni Karppi <karppitoni@gmail.com>
2019-02-06 10:25:08 +0100

    Change __main__ to use main in __init__

--- mullemeck/__main__.py
+++ mullemeck/__main__.py
@@ -1,19 +1,4 @@
-from mullemeck import app
-from dotenv import load_dotenv
-import os
-
-
-def validate_environment_variables():
-    github_secret = os.getenv('GITHUB_SECRET', default=None)
-    github_error_msg = 'GITHUB_SECRET environment variable required.'
-    assert github_secret is not None, github_error_msg
-
-
-def main(args):
-    load_dotenv()
-    validate_environment_variables()
-    debug = 'debug' in args
-    app.run(debug=debug)
+from mullemeck import main
 
 
 if __name__ == "__main__":
