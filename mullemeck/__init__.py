5450dcaedaa2812d1a3422f8e373e6ef9061f6da
Toni Karppi <karppitoni@gmail.com>
2019-02-06 10:26:17 +0100

    Add main in init

--- mullemeck/__init__.py
+++ mullemeck/__init__.py
@@ -1,3 +1,20 @@
 from mullemeck.routes import app
+from .db import create_tables
+from .settings import validate_environment_variables
+
+
+def main(*args):
+    validate_environment_variables()
+    assert len(args) == 1, 'Expected 1 argument <setup|develop|production>'
+    arg = args[0]
+    if arg == 'setup':
+        create_tables()
+    elif arg == 'develop':
+        app.run(debug=True)
+    elif arg == 'production':
+        app.run()
+    else:
+        raise ValueError(f'Unexpected argument: {arg}')
+
 
 __all__ = ['app']
