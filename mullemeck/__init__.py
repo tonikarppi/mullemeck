ac13ca117fde27e921d385b977c57bc161d5db4d
Toni Karppi <karppitoni@gmail.com>
2019-02-06 10:37:20 +0100

    Add feedback messages for setup functions

--- mullemeck/__init__.py
+++ mullemeck/__init__.py
@@ -1,6 +1,6 @@
 from mullemeck.routes import app
 from .db import create_tables
-from .settings import validate_environment_variables
+from .settings import validate_environment_variables, db_uri
 from .utils import add_samples_to_db
 
 
@@ -9,8 +9,10 @@ def main(*args):
     assert len(args) == 1, 'Expected 1 argument <setup|develop|production>'
     arg = args[0]
     if arg == 'create_tables':
+        print(f'Creating tables in: {db_uri}')
         create_tables()
     if arg == 'add_samples':
+        print(f'Adding sample entries to: {db_uri}')
         add_samples_to_db()
     elif arg == 'develop':
         app.run(debug=True)
