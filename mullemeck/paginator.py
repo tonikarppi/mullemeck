56f3d9fed3b16a50b3366500d76bb1a7fc30d917
Larsrat <lars.cg.lundin@gmail.com>
2019-02-07 22:05:13 +0100

    Forgot the pre-commit again...

--- mullemeck/paginator.py
+++ mullemeck/paginator.py
@@ -1,14 +1,14 @@
 class Paginator:
-  def __init__(self, list, per_page):
-    self.per_page = per_page
-    self.list = []
-    i = len(list)
-    j = 0
-    while(i > -per_page):
-      self.list.append([])
-      for y in range(0, per_page):
-        if((i-y) <= 0):
-          break
-        self.list[j].append(list[j*per_page+y])
-      j += 1
-      i -= per_page
+    def __init__(self, list, per_page):
+        self.per_page = per_page
+        self.list = []
+        i = len(list)
+        j = 0
+        while(i > -per_page):
+            self.list.append([])
+            for y in range(0, per_page):
+                if((i-y) <= 0):
+                    break
+                self.list[j].append(list[j*per_page+y])
+            j += 1
+            i -= per_page
