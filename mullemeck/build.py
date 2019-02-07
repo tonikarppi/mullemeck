47e96c43b9db0e8a4c0eec043f38d3960b698de1
SandstormVR <alexis.hubert@student.ecp.fr>
2019-02-07 15:32:33 +0000

    Fixed  #76

--- mullemeck/build.py
+++ mullemeck/build.py
@@ -129,37 +129,6 @@ def build_tests(directory):
 
     # Runs the build and gets the output in build object.
     build = subprocess.Popen('poetry run pytest ' + directory,
-                             shell=True, stdout=subprocess.PIPE)
-    # Waits for the process to end
-    build.wait()
-    success = build.returncode
-
-    # Reads the output and saves it in lines, then concatenates it in single
-    # string logs.
-    lines = []
-    for line in io.TextIOWrapper(build.stdout, encoding="utf-8"):
-        lines.append(line)
-    for line in io.TextIOWrapper(build.stderr, encoding="utf-8"):
-        lines.append(line)
-    logs = ' '.join(lines)
-
-    status = False
-    # If the shell command returns 0 it means that no errors occured. Every
-    # other value sets status to False.
-    if success == 0:
-        status = True
-
-    return status, logs
-
-
-def build_tests(directory):
-    """
-    This function runs pytest on a specific project located in `directory`.
-    It assumes that the project is compatible with the usage of pytest.
-    """
-
-    # Runs the build and gets the output in build object.
-    build = subprocess.Popen('poetry run pytest ' + directory,
                              shell=True, stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
     # Waits for the process to end
@@ -173,35 +142,6 @@ def build_tests(directory):
         lines.append(line)
     for line in io.TextIOWrapper(build.stderr, encoding="utf-8"):
         lines.append(line)
-    logs = ' '.join(lines)
-
-    status = False
-    # If the shell command returns 0 it means that no errors occured. Every
-    # other value sets status to False.
-    if success == 0:
-        status = True
-
-    return status, logs
-
-
-def build_tests(directory):
-    """
-    This function runs pytest on a specific project located in `directory`.
-    It assumes that the project is compatible with the usage of pytest.
-    """
-
-    # Runs the build and gets the output in build object.
-    build = subprocess.Popen('poetry run pytest ' + directory,
-                             shell=True, stdout=subprocess.PIPE)
-    # Waits for the process to end
-    build.wait()
-    success = build.returncode
-
-    # Reads the output and saves it in lines, then concatenates it in single
-    # string logs.
-    lines = []
-    for line in io.TextIOWrapper(build.stdout, encoding="utf-8"):
-        lines.append(line)  # or another encoding
     logs = ' '.join(lines)
 
     status = False
