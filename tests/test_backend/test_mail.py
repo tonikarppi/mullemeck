be608e1c15a63ec1b2598cab138134903bb77076
Alexander Westberg-Bladh <alexander.bladh@gmail.com>
2019-02-07 10:02:13 +0100

    Improve email test

--- tests/test_backend/test_mail.py
+++ tests/test_backend/test_mail.py
@@ -1,18 +1,27 @@
 from mullemeck.backend import email
+from flask_mail import Mail
 
 app = email.app
-app.config["TESTING"] = True
-mail = email.mail
+app.config['MAIL_DEFAULT_SENDER'] = "mulle@meck.net"
+app.config['MAIL_PORT'] = 1025
+app.config['MAIL_SERVER'] = "localhost"
+email.mail = Mail(app)
 
 
 def test_send_mail():
+    app.config["TESTING"] = True
+    email.mail = Mail(app)
 
     subject = "test"
     content = "<p> hi </p>"
     recipient = "bob@alicemail.net"
-    with mail.record_messages() as outbox:
+    with email.mail.record_messages() as outbox:
         email.send_mail(subject, content, recipient)
     assert len(outbox) == 1
     assert outbox[0].subject == subject
-    assert content in outbox[0].content
-    assert outbox[0].recipient == recipient
+    assert content in outbox[0].html
+    assert outbox[0].recipients == [recipient]
+
+
+def test_smtp_server_integration():
+    pass
