d4a1535cba185c85019e914dc84417bae1a46912
isakp <isakp@kth.se>
2019-02-10 17:17:13 +0100

    Implemented unit tests for build function

--- tests/test_build.py
+++ tests/test_build.py
@@ -5,44 +5,12 @@ from mullemeck.build import build_dependencies
 import os
 import subprocess
 import pytest
-
-
-def create_repos_for_unit_tests():
-    """
-    This function clones simple repos to run unit tests on the build.
-    It clones mullemeck-unit-tests repo and returns the directories in which
-    each relevant repo state is.
-    """
-    directories = {}
-    # repo with nothing but little dependencies to install
-    suc, logs, dir = clone_repo(
-        'https://github.com/SandstormVR/mullemeck-unit-tests',
-        '7ffb0ac9fe3fe05abdf688501d047ce501849a40')
-    directories['dir_dependencies_basics'] = dir
-    # Repo with pre-commit satisfied and no tests.
-    suc, logs, dir = clone_repo(
-        'https://github.com/SandstormVR/mullemeck-unit-tests',
-        'f676b450172207f1e65f5a7e0574c0cde7baba42')
-    directories['dir_hello_world_no_tests'] = dir
-    # Repo with pre_commit error and no tests
-    suc, logs, dir = clone_repo(
-        'https://github.com/SandstormVR/mullemeck-unit-tests',
-        'a82b9b829d352901e0f37702f87186ca3234d35a')
-    directories['dir_fail_hello_world_no_tests'] = dir
-    # Repo with valid test. Pre-commit runs
-    suc, logs, dir = clone_repo(
-        'https://github.com/SandstormVR/mullemeck-unit-tests',
-        '25e0e252636172a2d38b4ca8b3ada3c60b211596')
-    directories['dir_hello_world_success_test'] = dir
-    suc, logs, dir = clone_repo(
-        'https://github.com/SandstormVR/mullemeck-unit-tests',
-        '76bfd501cf59e6b9380abab5fc508208cf7a1692')
-    directories['dir_hello_world_fail_test'] = dir
-    print('Hello world')
-    return directories
-
-
-directories_unit_tests = create_repos_for_unit_tests()
+'''
+    This file runs unit-tests on the build functions.
+    It does so by cloning real and test repositories and
+    then testing the functions on them.
+    Test repository: 'https://github.com/SandstormVR/mullemeck-unit-tests'
+'''
 
 
 def test_clone_repo():
@@ -70,39 +38,85 @@ def test_clone_repo():
 
     success, logs, dir = clone_repo('https://github.com/hexadeciman/Snake',
                                     'e36d474082a8bddb0f04d114a24bdbbfc429a41b')
+    print(logs)
     assert success
     assert os.path.isdir(dir)
     subprocess.call('rm -rf ' + dir, shell=True)
 
 
 def test_build_dependencies():
+    '''
+        Clones a basic repo with nothing but some dependencies,
+        and tries to run build_dependencies on it. Should pass.
+    '''
+    # Clone repo with nothing but some dependecies to install
+    suc, logs, directory = clone_repo(
+        'https://github.com/SandstormVR/mullemeck-unit-tests',
+        '7ffb0ac9fe3fe05abdf688501d047ce501849a40')
+    success, logs = build_dependencies(directory)
 
-    success, logs = build_dependencies('path/to/nowhere')
-    assert not success
-    assert logs != ''
+    # Remove the cloned repos before assertions to be able to run tests again.
+    # Otherwise throw serror because the path
+    # already exists and directory isn't empty.
+    subprocess.call('rm -rf ' + directory, shell=True)
 
-    success, logs = build_dependencies(
-        directories_unit_tests['dir_dependencies_basics'])
     assert success
     assert logs != ''
 
 
 def test_build_static_checks():
-    success, logs = build_static_checks('random/path/to/nowhere')
-    assert not success
-    assert logs != ''
+    '''
+        Tests if build_static_checks (pre-commit check) works as intended
+    '''
+    # Repo with pre-commit satisfied and no tests.
+    suc, logs, success_directory = clone_repo(
+        'https://github.com/SandstormVR/mullemeck-unit-tests',
+        'f676b450172207f1e65f5a7e0574c0cde7baba42')
+
+    # Repo with pre_commit error and no tests
+    suc, logs, fail_directory = clone_repo(
+        'https://github.com/SandstormVR/mullemeck-unit-tests',
+        'a82b9b829d352901e0f37702f87186ca3234d35a')
+
+    success1, logs1 = build_static_checks(success_directory)
+    success2, logs2 = build_static_checks(fail_directory)
+
+    # Remove the cloned repos before assertions to be able to run tests again.
+    # Otherwise throw serror because the path
+    # already exists and directory isn't empty.
+    subprocess.call('rm -rf ' + success_directory, shell=True)
+    subprocess.call('rm -rf ' + fail_directory, shell=True)
+
+    assert success1
+    assert logs1 != ''
+    assert not success2
+    assert logs2 != ''
 
 
 def test_build_tests():
-    success, logs = build_tests('random/path/to/nowhere')
-    assert not success
-    assert logs != ''
+    '''
+        Tests if build_tests (pytest) works as intended
+    '''
+    # Repo with valid test. Pre-commit runs
+    suc, logs, success_directory = clone_repo(
+        'https://github.com/SandstormVR/mullemeck-unit-tests',
+        '25e0e252636172a2d38b4ca8b3ada3c60b211596')
 
+    # Repo with invalid test. Pre-commit runs
+    suc, logs, fail_directory = clone_repo(
+        'https://github.com/SandstormVR/mullemeck-unit-tests',
+        '76bfd501cf59e6b9380abab5fc508208cf7a1692')
 
-def test_remove_directories_unit_tests():
-    """
-    This function is executed in last as it removes all repos cloned for unit
-    testing purposes
-    """
-    for x in directories_unit_tests:
-        subprocess.call('rm -rf ' + directories_unit_tests[x], shell=True)
+    success1, logs1 = build_tests(success_directory)
+    success2, logs2 = build_tests(fail_directory)
+
+    # Remove the cloned repos before assertions to be able to run tests again.
+    # Otherwise throw serror because the path
+    # already exists and directory isn't empty.
+    subprocess.call('rm -rf ' + success_directory, shell=True)
+    subprocess.call('rm -rf ' + fail_directory, shell=True)
+
+    assert success1
+    assert logs1 != ''
+    assert not success2
+    assert logs2 != ''
